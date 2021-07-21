c5e80cdf6789b1ed011b7da4e0264c5bb81aaafe
hjf <947512302@qq.com>
2021-07-21 17:31:01 +0800

    initial nuxt

--- README.md
+++ README.md
@@ -1,19 +1,87 @@
-this is master
+# #撻著一句廣東歌歌詞 2021 海报生成器
 
-#### ToDoList
+<!-- This is a poster generator for ApacheCon Asia 2021. [[Run]](http://zhangwenli.com/acasia-poster/) -->
 
-- [ ] UI
-    - [x] ��ʼ����
-	- [ ] �������
-    - [x] ����
-	- [ ] �Զ�������
-- [ ] ����
-    - [ ] input
-    - [ ] ����ģ��
-- [ ] ������
-    - [ ] ����
-    - [ ] GFM task list 3-3
-- [ ] С����
-    - [ ] ����
+撻著廣東歌你可我可,自由DIY 
+<!-- [[Run]](http://zhangwenli.com/acasia-poster/) -->
 
-![#����һ��V�|����~|2020��4��24�ա�������](https://mmbiz.qpic.cn/mmbiz_jpg/iaaZjJ0oBoibx9JHIuuaQJpDOYdEiaTQjWIRt6VRwxEEgtFwicz8NkGrzVzu4Gu9LVOnKXibCmiaxXyMU5M6da9NiaDxw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1)
+It provides a form to input the name, title and other information so that each speaker of the event can generate their own poster easily with auto-completion.
+
+## Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project to `docs`
+$ npm run generate
+```
+
+## Adapt To Your Own Events
+
+1. Change `router.base` in `nuxt.config.js` to be the base of your server path
+2. Change the form in `pages/index.vue`.
+3. If you need auto-completion of the form, change `pages/data` and the logic with `trackZh` and `trackEn` in `pages/index.vue`.
+4. If you are looking for a tool to generate QR code with different reference information in the URL, please check out [Ovilia/qr-baker](https://github.com/Ovilia/qr-baker/blob/main/index.js).
+
+## Framework
+
+This repo uses [NuxtJS](https://nuxtjs.org/).
+
+For detailed explanation on how things work, check out the [documentation](https://nuxtjs.org).
+
+### Special Directories
+
+You can create the following extra directories, some of which have special behaviors. Only `pages` is required; you can delete them if you don't want to use their functionality.
+
+#### `assets`
+
+The assets directory contains your uncompiled assets such as Stylus or Sass files, images, or fonts.
+
+More information about the usage of this directory in [the documentation](https://nuxtjs.org/docs/2.x/directory-structure/assets).
+
+#### `components`
+
+The components directory contains your Vue.js components. Components make up the different parts of your page and can be reused and imported into your pages, layouts and even other components.
+
+More information about the usage of this directory in [the documentation](https://nuxtjs.org/docs/2.x/directory-structure/components).
+
+#### `layouts`
+
+Layouts are a great help when you want to change the look and feel of your Nuxt app, whether you want to include a sidebar or have distinct layouts for mobile and desktop.
+
+More information about the usage of this directory in [the documentation](https://nuxtjs.org/docs/2.x/directory-structure/layouts).
+
+
+#### `pages`
+
+This directory contains your application views and routes. Nuxt will read all the `*.vue` files inside this directory and setup Vue Router automatically.
+
+More information about the usage of this directory in [the documentation](https://nuxtjs.org/docs/2.x/get-started/routing).
+
+#### `plugins`
+
+The plugins directory contains JavaScript plugins that you want to run before instantiating the root Vue.js Application. This is the place to add Vue plugins and to inject functions or constants. Every time you need to use `Vue.use()`, you should create a file in `plugins/` and add its path to plugins in `nuxt.config.js`.
+
+More information about the usage of this directory in [the documentation](https://nuxtjs.org/docs/2.x/directory-structure/plugins).
+
+#### `static`
+
+This directory contains your static files. Each file inside this directory is mapped to `/`.
+
+Example: `/static/robots.txt` is mapped as `/robots.txt`.
+
+More information about the usage of this directory in [the documentation](https://nuxtjs.org/docs/2.x/directory-structure/static).
+
+#### `store`
+
+This directory contains your Vuex store files. Creating a file in this directory automatically activates Vuex.
+
+More information about the usage of this directory in [the documentation](https://nuxtjs.org/docs/2.x/directory-structure/store).
---
+++ pages/index.vue
@@ -0,0 +1,634 @@
+<template>
+  <el-row type="flex" class="poster-container" justify="center">
+    <el-col :span="12">
+      <div
+        id="poster-preview"
+        :style="{ cursor: imageUrl ? 'move' : 'default' }"
+        @mousedown.prevent="mouseDown($event)"
+        @mousemove.prevent="mouseMove($event)"
+        @mouseup.prevent="mouseUp($event)"
+      >
+        <img
+          class="author-img"
+          v-if="imageUrl"
+          :src="imageUrl"
+          ref="avatar"
+          :style="{
+            width: avatarPos.width + 'vh',
+            height: avatarPos.height + 'vh',
+            left: avatarPos.left + 'vh',
+            top: avatarPos.top + 'vh',
+          }"
+        />
+        <div class="bg"></div>
+        <img
+          class="poster-template"
+          :src="'poster-template-' + lang + '.png'"
+        />
+        <div class="poster-content">
+          <div
+            class="title"
+            :style="{ 'font-size': 3.7 * titleFontSize + 'vh' }"
+            v-html="'“ ' + title + ' ”'"
+          ></div>
+          <div class="name" :style="{ 'font-size': 3.7 * nameFontSize + 'vh' }">
+            {{ name }}
+          </div>
+          <div
+            class="topic"
+            :style="{ 'font-size': 2.3 * topicFontSize + 'vh' }"
+          >
+            {{ topic }}_{{ singer }}
+          </div>
+          <div class="time">
+            <span>{{ melodist }}</span>
+            <span>{{ time }}</span>
+          </div>
+          <img
+            class="keynote"
+            :src="'keynote-' + lang + '.png'"
+            v-if="isKeynote"
+          />
+        </div>
+      </div>
+    </el-col>
+    <el-col
+      :span="8"
+      class="poster-control"
+      v-loading="isDownloading"
+      element-loading-text="生成海报中"
+    >
+      <el-row>
+        <h1>#撻著一句廣東歌歌詞 2021 海报生成器</h1>
+        <!-- <el-radio-group size="small" v-model="lang">
+          <el-radio-button label="zh"></el-radio-button>
+          <el-radio-button label="en"></el-radio-button>
+        </el-radio-group> -->
+      </el-row>
+      <el-form>
+        <!-- <el-form-item label="论坛名称" id="track">
+          <el-autocomplete
+            v-model="track"
+            :fetch-suggestions="trackQuery"
+          ></el-autocomplete>
+        </el-form-item> -->
+        <el-form-item label="来自哪首歌?">
+          <!-- <el-checkbox v-model="isKeynote">主题演讲</el-checkbox> -->
+          <el-input v-model="topic" />
+        </el-form-item>
+        <el-form-item label="歌词">
+          <el-input type="textarea" v-model="title" />
+        </el-form-item>
+        <el-form-item label="填词" style="margin-top: 2vw">
+          <el-autocomplete
+            style="width:100%"
+            v-model="name"
+            :fetch-suggestions="nameQuery"
+            @select="nameSelect"
+          ></el-autocomplete>
+        </el-form-item>
+        <el-form-item label="作曲">
+          <el-input v-model="melodist" />
+        </el-form-item>
+        <el-form-item label="歌手">
+          <el-input v-model="singer" />
+        </el-form-item>
+        <el-form-item label="日期">
+          <el-input v-model="time" />
+        </el-form-item>
+        <el-form-item
+          :label="'特别符号' + (imageUrl ? '（可在海报中拖动）' : '')"
+        >
+          <el-col :span="3">
+            <input
+              type="file"
+              ref="avatarInput"
+              style="display: none"
+              @change="avatarChange"
+            />
+            <a class="avatar-uploader" href="javascript:;" @click="setAvatar()">
+              <i class="el-icon-plus avatar-icon"></i>
+            </a>
+          </el-col>
+          <el-col :span="9" class="icon-panel">
+            <a href="javascript:;" @click="zoomIn()">
+              <i class="el-icon-zoom-in avatar-icon" v-if="imageUrl"></i>
+            </a>
+            <a href="javascript:;" @click="zoomOut()">
+              <i class="el-icon-zoom-out avatar-icon" v-if="imageUrl"></i>
+            </a>
+            <a href="javascript:;" @click="zoomReset()">
+              <i class="el-icon-refresh-left avatar-icon" v-if="imageUrl"></i>
+            </a>
+          </el-col>
+        </el-form-item>
+        <!-- <el-form-item label="发布时间">
+          <el-input v-model="time" />
+        </el-form-item> -->
+        <el-form-item label="字号调整（歌词）">
+          <div :span="12">
+            <a href="javascript:;" @click="titleFontAdd()">
+              <i class="el-icon-zoom-in avatar-icon"></i>
+            </a>
+            <a href="javascript:;" @click="titleFontMinus()">
+              <i class="el-icon-zoom-out avatar-icon"></i>
+            </a>
+            <a href="javascript:;" @click="titleFontReset()">
+              <i class="el-icon-refresh-left avatar-icon"></i>
+            </a>
+          </div>
+        </el-form-item>
+        <el-form-item label="字号调整（填词）">
+          <div :span="12">
+            <a href="javascript:;" @click="nameFontAdd()">
+              <i class="el-icon-zoom-in avatar-icon"></i>
+            </a>
+            <a href="javascript:;" @click="nameFontMinus()">
+              <i class="el-icon-zoom-out avatar-icon"></i>
+            </a>
+            <a href="javascript:;" @click="nameFontReset()">
+              <i class="el-icon-refresh-left avatar-icon"></i>
+            </a>
+          </div>
+        </el-form-item>
+        <el-form-item label="字号调整（from）">
+          <div :span="12">
+            <a href="javascript:;" @click="topicFontAdd()">
+              <i class="el-icon-zoom-in avatar-icon"></i>
+            </a>
+            <a href="javascript:;" @click="topicFontMinus()">
+              <i class="el-icon-zoom-out avatar-icon"></i>
+            </a>
+            <a href="javascript:;" @click="topicFontReset()">
+              <i class="el-icon-refresh-left avatar-icon"></i>
+            </a>
+          </div>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="download()"> 生成海报 </el-button>
+        </el-form-item>
+
+        <el-form-item class="info">
+          <i class="el-icon-loading"></i> 多谢靓女<a href="http://github.com/Ovilia">@Ovilia</a>的模板 设计版权<a
+            href="https://weibo.com/u/6675339465"
+            >@撻著廣東歌</a
+          >所有 <a href="mailto:oviliazhang@gmail.com">问题反馈</a>
+        </el-form-item>
+        <el-form-item class="info">
+          支持广东歌就系支持紧你自己！
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+// @ts-ignore
+import domtoimage from "retina-dom-to-image";
+import trackZhRaw from "./data/track-zh";
+import trackEnRaw from "./data/track-en";
+import trackKeyRaw from "./data/track-keynote";
+
+type SpeechInfo = {
+  track: string;
+  title: string;
+  name: string;
+  topic: string;
+  time: string;
+  isKeynote: boolean;
+};
+
+const trackKey = trackKeyRaw
+  .split("\n")
+  .slice(2)
+  .filter((line) => !!line);
+
+function getTrackInfo(raw: string, isZh: boolean): SpeechInfo[] {
+  let infos = raw
+    .split("\n")
+    .slice(2)
+    .filter((line) => !!line)
+    .map((line) => {
+      const arr = line.split(",");
+      return {
+        track: arr[2],
+        title: arr[4],
+        name: arr[1],
+        topic: arr[3],
+        time: arr[5],
+        isKeynote: false,
+      };
+    });
+
+  // add keynote
+  infos = infos.concat(
+    trackKey
+      .filter((line) => {
+        return isZh === /[\u4e00-\u9fa5]/.test(line);
+      })
+      .map((line) => {
+        const arr = line.split(",");
+        return {
+          track: arr[0],
+          title: arr[2],
+          name: arr[1],
+          topic: arr[3],
+          time: arr[4],
+          isKeynote: true,
+        };
+      })
+  );
+
+  return infos;
+}
+
+const trackZh = getTrackInfo(trackZhRaw, true);
+const trackEn = getTrackInfo(trackEnRaw, false);
+
+export default Vue.extend({
+  data() {
+    return {
+      track: "",
+      imageUrl: null as unknown as string,
+      title: "",
+      name: "",
+      topic: "",
+      time: "",
+      melodist: "",
+      singer: "",
+      isKeynote: false,
+      avatarInput: null,
+
+      titleFontSize: 1,
+      nameFontSize: 1,
+      topicFontSize: 1,
+
+      avatarDefaultPos: {
+        width: 0,
+        height: 0,
+        top: 0,
+        left: 0,
+      },
+      avatarPos: {
+        width: 0,
+        height: 0,
+        top: 0,
+        left: 0,
+      },
+      avatarZoom: 1,
+      isMouseDown: false,
+      mouseX: 0,
+      mouseY: 0,
+      isDownloading: false,
+      posterBase64: "",
+
+      lang: "zh",
+    };
+  },
+
+  mounted() {},
+
+  methods: {
+    trackQuery(str: string, cb: Function) {
+      const trackInfos = this.lang === "zh" ? trackZh : trackEn;
+      const result = str
+        ? trackInfos.filter((info) => info.track === str)
+        : trackInfos;
+      const distinct = result
+        .map((info) => info.track)
+        .filter((track, index, self) => self.indexOf(track) === index);
+      cb(
+        distinct.map((track) => {
+          return { value: track };
+        })
+      );
+    },
+
+    nameQuery(str: string, cb: Function) {
+      const trackInfos = this.lang === "zh" ? trackZh : trackEn;
+      const result = str
+        ? trackInfos.filter((info) => info.name === str)
+        : trackInfos.filter((info) => info.track === this.track);
+      cb(
+        result.map((track) => {
+          return {
+            value: track.name,
+            track,
+          };
+        })
+      );
+    },
+
+    nameSelect(item: { track: SpeechInfo; value: string }) {
+      this.topic = item.track.topic;
+      this.time = item.track.time;
+      this.title = item.track.title;
+      this.isKeynote = item.track.isKeynote;
+      this.nameFontReset();
+      this.topicFontReset();
+    },
+
+    download() {
+      this.isDownloading = true;
+      domtoimage
+        .toJpeg(document.getElementById("poster-preview"))
+        .then((url: string) => {
+          this.posterBase64 = url;
+          this.isDownloading = false;
+          const link = document.createElement("a");
+          link.href = url;
+          link.download = this.name + ".jpeg";
+          link.click();
+        });
+    },
+
+    mouseDown(event: MouseEvent) {
+      if (!this.imageUrl) {
+        return;
+      }
+      this.isMouseDown = true;
+      this.mouseX = event.pageX;
+      this.mouseY = event.pageY;
+    },
+
+    mouseMove(event: MouseEvent) {
+      if (!this.isMouseDown) {
+        return;
+      }
+      const ratio = 100 / window.innerHeight;
+
+      this.avatarPos.left += (event.pageX - this.mouseX) * ratio;
+      this.avatarPos.top += (event.pageY - this.mouseY) * ratio;
+      this.mouseX = event.pageX;
+      this.mouseY = event.pageY;
+    },
+
+    mouseUp(event: MouseEvent) {
+      this.isMouseDown = false;
+    },
+
+    zoomIn() {
+      this.avatarZoom += 0.05;
+      this.avatarPos.width = this.avatarDefaultPos.width * this.avatarZoom;
+      this.avatarPos.height = this.avatarDefaultPos.height * this.avatarZoom;
+    },
+
+    zoomOut() {
+      this.avatarZoom -= 0.05;
+      this.avatarPos.width = this.avatarDefaultPos.width * this.avatarZoom;
+      this.avatarPos.height = this.avatarDefaultPos.height * this.avatarZoom;
+    },
+
+    zoomReset() {
+      this.avatarZoom = 1;
+      this.avatarPos.width = this.avatarDefaultPos.width;
+      this.avatarPos.height = this.avatarDefaultPos.height;
+      this.avatarPos.left = this.avatarDefaultPos.left;
+      this.avatarPos.top = this.avatarDefaultPos.top;
+    },
+
+    titleFontAdd() {
+      this.titleFontSize += 0.1;
+    },
+
+    titleFontMinus() {
+      this.titleFontSize -= 0.1;
+    },
+
+    titleFontReset() {
+      this.titleFontSize = 1;
+    },
+
+    nameFontAdd() {
+      this.nameFontSize += 0.1;
+    },
+
+    nameFontMinus() {
+      this.nameFontSize -= 0.1;
+    },
+
+    nameFontReset() {
+      this.nameFontSize = 1;
+    },
+
+    topicFontAdd() {
+      this.topicFontSize += 0.1;
+    },
+
+    topicFontMinus() {
+      this.topicFontSize -= 0.1;
+    },
+
+    topicFontReset() {
+      this.topicFontSize = 1;
+    },
+
+    setAvatar() {
+      if (this.$refs.avatarInput) {
+        (this.$refs.avatarInput as HTMLElement).click();
+      }
+    },
+
+    // @ts-ignore
+    avatarChange(event) {
+      const file =
+        event.target.files && event.target.files.length
+          ? event.target.files[0]
+          : null;
+      if (file) {
+        this.imageUrl = window.URL.createObjectURL(file);
+
+        const img = new Image();
+        img.onload = () => {
+          const h = (417 * 100) / 2208;
+          const w = (h / img.height) * img.width;
+          const top = (373 / 2208) * 100;
+          const pw = (100 / 2208) * 1242;
+          const left = (pw - w) / 2;
+          this.avatarDefaultPos.width = this.avatarPos.width = w;
+          this.avatarDefaultPos.height = this.avatarPos.height = h;
+          this.avatarDefaultPos.left = this.avatarPos.left = left;
+          this.avatarDefaultPos.top = this.avatarPos.top = top;
+        };
+        img.src = this.imageUrl;
+      }
+    },
+  },
+});
+</script>
+
+<style>
+@font-face {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: bold;
+  src: url(~assets/OpenSans-Bold.ttf) format("truetype");
+}
+@font-face {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: normal;
+  src: url(~assets/OpenSans-Light.ttf) format("truetype");
+}
+@font-face {
+  font-family: "SourceHanSerifSC";
+  font-style: normal;
+  font-weight: 300;
+  src: url(~assets/SourceHanSerifSC-Heavy.otf) format("opentype");
+}
+
+h1 {
+  margin-bottom: 15px;
+  font-size: 20px;
+  font-family: "SourceHanSerifSC", "Open Sans";
+}
+
+.poster-container {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  font-family: "Open Sans";
+}
+
+#poster-preview {
+  position: relative;
+  width: max-content;
+  height: 100vh;
+  overflow: hidden;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+.bg {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background: white;
+  z-index: -20;
+}
+
+.poster-template {
+  height: 100vh;
+}
+
+.poster-content {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  padding: 12vh 3vh 0 3vh;
+  text-align: center;
+  color: #fff;
+  font-size: 2vh;
+}
+
+.author-img {
+  position: absolute;
+  z-index: 99;
+}
+
+.title {
+  font-weight: normal;
+  color: #ffe342;
+  height: 36vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "SourceHanSerifSC", "Open Sans";
+}
+
+.name {
+  margin: 4vh 5vh 0.5vh 0;
+  color: #ffe342;
+  font-family: "SourceHanSerifSC", "Open Sans";
+  font-size: 3.7vh;
+  height: 29vh;
+  font-weight: bold;
+  text-align: right;
+}
+
+.topic {
+  padding-right: 5vh;
+  font-size: 2.3vh;
+  font-weight: bold;
+  text-align: right;
+  height: 4.5vh;
+  margin-top: 3vh;
+}
+
+.time {
+  font-size: 2vh;
+  color: #ccc;
+  margin-top: 1vh;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 5vh 0 6vh;
+  box-sizing: border-box;
+}
+
+.keynote {
+  display: block;
+  margin: 0.5vh auto;
+  height: 5vh;
+}
+
+.el-form-item {
+  margin-bottom: 5px;
+}
+
+#track {
+  margin-top: 10px;
+}
+
+.avatar-uploader {
+  display: inline-block;
+  margin-top: 10px;
+  width: 40px;
+  height: 40px;
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.avatar-uploader:hover {
+  border-color: #409eff;
+}
+
+.avatar-icon {
+  font-size: 22px;
+  color: #ccc;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+}
+
+.icon-panel {
+  margin-top: 12px;
+}
+
+.el-upload:hover .avatar-icon,
+a:hover .avatar-icon {
+  color: #409eff;
+}
+
+.avatar {
+  width: 40px;
+  height: 40px;
+  display: block;
+}
+
+.info,
+.info a {
+  color: #aaa;
+}
+</style>
